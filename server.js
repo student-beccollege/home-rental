dc88142987f4c98fb70e3ad138ea355bec0f7181
avinash <avinashkesanur@gmail.com>
2025-01-29 10:47:59 +0530

    Updated renter registration to use hashed password

--- server.js
+++ server.js
@@ -102,9 +102,7 @@ app.post('/register-renter', async (req, res) => {
       return res.status(400).send('Email already registered');
     }
 
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
+   
     const newRenter = new Renter({
       fname,
       email,
